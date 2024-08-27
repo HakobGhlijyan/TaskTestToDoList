c93c70b1ec078163865cb827340aa3910b324728
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-08-27 12:46:14 +0400

    Initial Commit

---
+++ TaskTestToDoList/TaskTestToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTestToDoListApp.swift
+//  TaskTestToDoList
+//
+//  Created by Hakob Ghlijyan on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskTestToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
